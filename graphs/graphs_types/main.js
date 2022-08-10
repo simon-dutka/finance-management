122db7cac70cb57643cc5b8ee8200fe45338fdbf
Simon Dutka <SimonDutkaWork@gmail.com>
2022-08-10 16:52:38 +0200

    feat: added adding categories to main graph

--- graphs/graphs_types/main.js
+++ graphs/graphs_types/main.js
@@ -1,11 +1,30 @@
 const main = () => {
-    let data = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    // Todo: Add data with values of categories
+
+    const expenseAddBtn = document.querySelector('#expense__add-button');
+
+    let labelsData = [];
+
+    let labelsDataLocalStorage = JSON.parse(
+        localStorage.getItem('mainGraphLabelsData')
+    );
+
+    if (labelsDataLocalStorage !== null) labelsData = labelsDataLocalStorage;
+
+    const data = {
+        labels: labelsData,
         datasets: [
             {
-                backgroundColor: 'rgb(64, 189, 152)',
-                borderColor: 'rgb(64, 189, 152)',
-                data: [0, 50, 60, 76, 162, 200, 320],
+                data: [0, 0, 0, 0, 0, 0],
+                backgroundColor: [
+                    '#75d18e',
+                    '#75d1b5',
+                    '#75afd1',
+                    '#d18c75',
+                    '#d1759a',
+                    '#af75d1',
+                ],
+                hoverOffset: 4,
             },
         ],
     };
@@ -24,6 +43,41 @@ const main = () => {
     };
 
     const mainGraph = new Chart(document.getElementById('main-graph'), config);
+
+    expenseAddBtn.addEventListener('click', () => {
+        // Expense data from localStorare
+        let expenseTransactionsText;
+
+        // All categories in graph
+        let allCategories = [
+            'Food',
+            'Education',
+            'House',
+            'Transport',
+            'Entertainment',
+            'Other',
+        ];
+
+        expenseTransactionsText = localStorage.getItem('expenseTransactions');
+
+        // Adding data to labelsData
+        allCategories.forEach((category) => {
+            if (expenseTransactionsText !== null) {
+                if (
+                    expenseTransactionsText.includes(category) &&
+                    labelsData.includes(category) === false
+                ) {
+                    labelsData.push(category);
+                }
+            }
+        });
+
+        // Update data labels
+        mainGraph.data.labels = labelsData;
+        mainGraph.update();
+
+        localStorage.setItem('mainGraphLabelsData', JSON.stringify(labelsData));
+    });
 };
 
 export default main;
