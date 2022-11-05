8ed8a59f177328ff98a3168027498420fdee6de7
Simon Dutka <SimonDutkaWork@gmail.com>
2022-11-05 22:50:10 +0200

    Changed let to const

--- client/scripts/dist/transactions/OpenTransaction.js
+++ client/scripts/dist/transactions/OpenTransaction.js
@@ -1,9 +1,9 @@
 const openTransaction = () => {
-    let cards = document.querySelectorAll('.card__item');
-    let forms = document.querySelectorAll('.form__transactions');
+    const cards = document.querySelectorAll('.card__item');
+    const forms = document.querySelectorAll('.form__transactions');
     cards.forEach((element, i) => {
         element.addEventListener('click', () => {
-            let classOpen = 'form__transactions--open';
+            const classOpen = 'form__transactions--open';
             if (i === 0) {
                 forms[1].classList.remove(classOpen);
                 forms[i].classList.toggle(classOpen);
