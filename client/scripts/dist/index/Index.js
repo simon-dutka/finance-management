b1dbbef88742717d9d34912a0b214ed06524fe20
Simon Dutka <SimonDutkaWork@gmail.com>
2022-11-06 23:02:45 +0100

    Added rendering transactions to index.html history component
    -Moved RenderTransactions.ts to transactions folder

--- client/scripts/dist/index/Index.js
+++ client/scripts/dist/index/Index.js
@@ -1,4 +1,6 @@
 import { loader } from '../Loader.js';
 import { navbarTemplate } from '../templates/Navbar.js';
+import { renderTransactions } from '../transactions/RenderTransactions.js';
 loader();
 navbarTemplate();
+renderTransactions();
--- client/scripts/src/history/history.ts
+++ client/scripts/src/history/history.ts
@@ -1,6 +1,6 @@
 import { loader } from '../Loader.js';
 import { navbarTemplate } from '../templates/Navbar.js';
-import { renderTransactions } from './RenderTransactions.js';
+import { renderTransactions } from '../transactions/RenderTransactions.js';
 
 loader();
 navbarTemplate();
