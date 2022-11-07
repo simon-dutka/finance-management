ce905043732784684e769c0e54363364d96c6504
Simon Dutka <SimonDutkaWork@gmail.com>
2022-11-07 21:17:16 +0100

    Fixed transactions forms validation

--- client/scripts/src/transactions/AddTransaction.ts
+++ client/scripts/src/transactions/AddTransaction.ts
@@ -7,64 +7,60 @@ const addTransaction = () => {
 
     formBtns.forEach((element, i) => {
         element.addEventListener('click', () => {
-            const saveTransaction = () => {
-                let createdTransactions = localStorage.getItem('transactions');
+            if (formTransactions[0].checkValidity() === true) {
+                const saveTransaction = () => {
+                    let createdTransactions =
+                        localStorage.getItem('transactions');
 
-                let transactions: {}[];
-                createdTransactions === null
-                    ? (transactions = [])
-                    : (transactions = JSON.parse(createdTransactions));
+                    let transactions: {}[];
+                    createdTransactions === null
+                        ? (transactions = [])
+                        : (transactions = JSON.parse(createdTransactions));
 
-                // Prototype of transaction object
-                const transactionProto = {
-                    title: 'Title of transaction',
-                    amount: 1,
-                    category: 'Salary',
-                    source: 'Cash',
-                    type: 'Income',
+                    // Prototype of transaction object
+                    const transactionProto = {
+                        title: 'Title of transaction',
+                        amount: 1,
+                        category: 'Salary',
+                        source: 'Cash',
+                        type: 'Income',
+                    };
+
+                    let newTransaction = Object.create(transactionProto);
+
+                    // Adding values to transaction object properties
+                    let j = 0;
+                    for (let property in newTransaction) {
+                        newTransaction[property] =
+                            formTransactions[i].children[j].value;
+                        j++;
+                    }
+
+                    // Add $ symbol to amount property
+                    newTransaction.amount = `${newTransaction.amount}$`;
+
+                    // Add type of transaction
+                    i === 0
+                        ? (newTransaction.type = 'Income')
+                        : (newTransaction.type = 'Expense');
+
+                    transactions.push(newTransaction);
+
+                    return JSON.stringify(transactions);
                 };
 
-                let newTransaction = Object.create(transactionProto);
-
-                // Adding values to transaction object properties
-                let j = 0;
-                for (let property in newTransaction) {
-                    newTransaction[property] =
-                        formTransactions[i].children[j].value;
-                    j++;
-                }
-
-                // Add $ symbol to amount property
-                newTransaction.amount = `${newTransaction.amount}$`;
-
-                // Add type of transaction
-                i === 0
-                    ? (newTransaction.type = 'Income')
-                    : (newTransaction.type = 'Expense');
-
-                transactions.push(newTransaction);
-
-                return JSON.stringify(transactions);
-            };
-
-            const clearInputs = () => {
-                for (let j = 0; j < 4; j++) {
-                    formTransactions[i].children[j].value = '';
-                }
-            };
-
-            // Checking if password length is < 100
-            if (formTransactions[i].children[0].value.length <= 100) {
-                // Checking if amount length is < 7
-                if (formTransactions[i].children[1].value.length <= 7) {
-                    localStorage.setItem('transactions', saveTransaction());
-                    clearInputs();
-                    closeTransactions(i);
+                // Checking if password length is < 100
+                if (formTransactions[i].children[0].value.length <= 100) {
+                    // Checking if amount length is < 7
+                    if (formTransactions[i].children[1].value.length <= 7) {
+                        localStorage.setItem('transactions', saveTransaction());
+                        closeTransactions(i);
+                    } else {
+                        alert('Amount must be shorter than 7 digits');
+                    }
                 } else {
-                    alert('Amount must be shorter than 7 digits');
+                    alert('Password must be shorter than 100 letters');
                 }
-            } else {
-                alert('Password must be shorter than 100 letters');
             }
         });
     });
