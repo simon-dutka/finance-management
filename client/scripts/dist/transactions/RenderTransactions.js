cc00eaa74e3bd3bd534394f1717400e5a03b6e9b
Simon Dutka <SimonDutkaWork@gmail.com>
2022-11-07 15:00:18 +0100

    Added shortening titles in history.html

--- client/scripts/dist/transactions/RenderTransactions.js
+++ client/scripts/dist/transactions/RenderTransactions.js
@@ -5,6 +5,15 @@ const renderTransactions = () => {
         for (let transaction of createdTransactions) {
             let historyItemContainer = document.createElement('div');
             historyItemContainer.classList.add('history__item-container');
+            // Shortening titles longer than 27 characters
+            if (transaction.title.length > 27) {
+                let shortTitle = [];
+                for (let i = 0; i < 27; i++) {
+                    shortTitle.push(transaction.title.split('')[i]);
+                }
+                shortTitle.push('...');
+                transaction.title = shortTitle.toString().replace(/,/g, '');
+            }
             // Create html element
             Object.values(transaction).forEach((i) => {
                 let item = document.createElement('p');
